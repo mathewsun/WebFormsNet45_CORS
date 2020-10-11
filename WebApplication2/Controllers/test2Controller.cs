e541113ca9fe2bc9ec7535ddba3d05f71ffb01ce
matthew <mathew.dubrovin@gmail.com>
2020-10-12 01:36:09 +0300

    add fix

--- WebApplication2/Controllers/test2Controller.cs
+++ WebApplication2/Controllers/test2Controller.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApplication2.Controllers
 {
+    [EnableCors(origins: "http://example.com/", headers: "*", methods: "*")]
     public class test2Controller : ApiController
     {
         // GET: api/test2
