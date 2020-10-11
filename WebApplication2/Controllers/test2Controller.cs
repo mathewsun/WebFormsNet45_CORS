f54c7f5ca39510883a91360488cf5704ed54f682
matthew <mathew.dubrovin@gmail.com>
2020-10-11 15:26:51 +0300

    init

---
+++ WebApplication2/Controllers/test2Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class test2Controller : ApiController
+    {
+        // GET: api/test2
+        public IHttpActionResult Get()
+        {
+            var result = new string[] { "value1", "value2" };
+
+            return Json(result);
+        }
+
+        // GET: api/test2/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/test2
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/test2/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/test2/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication2/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+
+namespace WebApplication2
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // Код, выполняемый при запуске приложения
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
